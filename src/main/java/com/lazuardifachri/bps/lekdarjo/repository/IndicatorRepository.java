2430ee7cd43f9ec04da1cfb9f501aea8ee1b8d91
Fachri_izzudin <izzudin609@gmail.com>
2021-03-28 05:42:20 +0700

    update order by in repo indicator

--- src/main/java/com/lazuardifachri/bps/lekdarjo/repository/IndicatorRepository.java
+++ src/main/java/com/lazuardifachri/bps/lekdarjo/repository/IndicatorRepository.java
@@ -15,16 +15,18 @@ public interface IndicatorRepository extends JpaRepository<Indicator, Long> {
 
     String ORDER = " ORDER by i.releaseDate DESC";
 
+    String ORDERID = " ORDER by i.id DESC";
+
     @Query(QUERY + ORDER)
     Page<Indicator> findAllInfo(Pageable pageable);
 
     @Query(QUERY + "WHERE i.id = :id")
     Optional<Indicator> findByIdInfo(@Param("id") Long  id);
 
-    @Query(QUERY + "WHERE i.category.id = :categoryId")
+    @Query(QUERY + "WHERE i.category.id = :categoryId" + ORDERID)
     Page<Indicator> findByCategory(@Param("categoryId") Long categoryid, Pageable pageable);
 
-    @Query(QUERY + "WHERE i.category.subject.id = :subjectId")
+    @Query(QUERY + "WHERE i.category.subject.id = :subjectId" + ORDERID)
     Page<Indicator> findBySubject(@Param("subjectId") Long subjectId, Pageable pageable);
 
     Boolean existsByTitle(String title);
